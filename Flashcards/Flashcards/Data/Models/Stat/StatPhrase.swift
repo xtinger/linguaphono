e456f0220816437a003617e9f181387d1473f80e
xtinger <xtinger@bk.ru>
2018-05-21 18:10:04 +0300

    phrase: Hashable, CustomStringConvertible, CustomDebugStringConvertible

--- Flashcards/Flashcards/Data/Models/Stat/StatPhrase.swift
+++ Flashcards/Flashcards/Data/Models/Stat/StatPhrase.swift
@@ -12,13 +12,17 @@ enum AnswerType : Int, Codable{
     case Known = 0, Unknown
 }
 
-struct StatPhrase: Codable, Equatable {
+struct StatPhrase: Codable, Equatable, Hashable {
 
     var textEng : String
     var textRu : String
     var lastAnswers : [AnswerType]
     var corrects : Int
     var incorrects : Int
+    
+    var hashValue: Int {
+        return textEng.hashValue ^ textRu.hashValue
+    }
     
     init(textEng: String, textRu: String, corrects: Int, incorrects: Int) {
         self.textEng = textEng
@@ -31,10 +35,10 @@ struct StatPhrase: Codable, Equatable {
 
 extension StatPhrase: CustomStringConvertible, CustomDebugStringConvertible {
     var debugDescription: String {
-        return "\(textEng) : \(textRu) [\(corrects):\(incorrects)]"
+        return "\(textEng) : \(textRu) [\(corrects):\(incorrects)]\n"
     }
     
     var description: String {
-        return "\(textEng) : \(textRu) [\(corrects):\(incorrects)]"
+        return "\(textEng) : \(textRu) [\(corrects):\(incorrects)]\n"
     }
 }
