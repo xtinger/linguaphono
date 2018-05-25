a9d193d43af411c9e5dc8a7eaa3f681e836596d0
xtinger <xtinger@bk.ru>
2018-05-25 14:41:20 +0300

    настройки

--- Flashcards/Flashcards/Settings/MenuVC.swift
+++ Flashcards/Flashcards/Settings/MenuVC.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class MenuVC: UIViewController {
+    
+    var wireframe: FlashcardsWireframe?
+    
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var muteSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        urlTextField.text = GameConfig.phrasesURL?.absoluteString
+        muteSwitch.isOn = !GameConfig.muted
+    }
+    
+    @IBAction func buttonLoadTouched(_ sender: Any) {
+        if let string = urlTextField.text, let phrasesURL = URL(string: string) {
+            GameConfig.phrasesURL = phrasesURL
+            wireframe?.reload()
+        }
+    }
+    
+    @IBAction func muteSwitchValueChanged(_ sender: Any) {
+        GameConfig.muted = !muteSwitch.isOn
     }
 }
